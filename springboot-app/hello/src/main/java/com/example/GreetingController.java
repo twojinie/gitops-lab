6d245066869bdb377da3ee9d89dc4905d83edae9
EC2 Default User <ec2-user@ip-172-31-26-18.ap-southeast-1.compute.internal>
2026-05-02 05:47:12 +0000

    소스 코드 수정

--- springboot-app/hello/src/main/java/com/example/GreetingController.java
+++ springboot-app/hello/src/main/java/com/example/GreetingController.java
@@ -9,11 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "Hello, %s ^___^";
 	private final AtomicLong counter = new AtomicLong();
 
 	@GetMapping("/hello")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "New World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 }
